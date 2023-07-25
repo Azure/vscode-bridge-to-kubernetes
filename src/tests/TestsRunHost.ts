738095cc43b3a16c237ec02a8fb7bdd290070104
Vidya Reddy <59590642+Vidya2606@users.noreply.github.com>
2023-07-25 10:40:45 -0700

    updating the vscode test extension

--- src/tests/TestsRunHost.ts
+++ src/tests/TestsRunHost.ts
@@ -4,7 +4,7 @@
 'use strict';
 
 import * as path from 'path';
-import { runTests } from 'vscode-test';
+import { runTests } from '@vscode/test-electron';
 
 async function main(): Promise<void> {
     try {
@@ -14,13 +14,14 @@ async function main(): Promise<void> {
         // The path to the extension test script.
         const extensionTestsPath: string = path.resolve(__dirname, `./TestsRunner`);
 
-        // Download VS Code, unzip it and run the tests.
-        // Using old version of vscode instead of 1.47 to run the tests, as 1.47 version has issues.
-        await runTests({ version: `1.46.1`, extensionDevelopmentPath, extensionTestsPath });
+        await runTests({
+			extensionDevelopmentPath,
+			extensionTestsPath,
+		});
     }
     catch (error) {
         // tslint:disable-next-line: no-console
-        console.error(`Failed to run tests`);
+        console.error(`Failed to run tests`, error);
         process.exit(1);
     }
 }

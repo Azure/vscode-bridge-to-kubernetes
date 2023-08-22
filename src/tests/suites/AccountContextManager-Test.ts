7f2d2406cc1d51e3cc7dea3ac3320d71a7b97fad
Hariharan Subramanian <hsubramanian@microsoft.com>
2023-08-22 15:04:07 +0000

    fix account context tests

--- src/tests/suites/AccountContextManager-Test.ts
+++ src/tests/suites/AccountContextManager-Test.ts
@@ -6,7 +6,13 @@ import { loggerStub } from "../CommonTestObjects";
 describe('AccountContextManagerTest', () => {
     it('should return kube config path', async() => {
         let accountContextManager: AccountContextManager = new AccountContextManager(loggerStub);
-        const result = await accountContextManager.getKubeconfigPathAsync();
-        expect(result).not.to.be.null;
+        try {
+            const result = await accountContextManager.getKubeconfigPathAsync();
+            // this happens when debugging tests when k8s extension is installed.
+            expect(result).not.to.be.null;
+        } catch(error) {
+            // this will happen if k8s extension is not installed during CI/CD
+            expect(error.message).to.be.equal('The API to get the kubeconfig path from the Kubernetes extension is not available');
+        }
     });
 });
